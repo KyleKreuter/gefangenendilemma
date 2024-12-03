393bbae87c1ee8c847617ba12bab0dfd13e7bdb0
kylekreuter <kyle.kreuter@outlook.de>
2024-12-03 16:32:45 +0100

    fix default config is not loaded because file is not present

--- src/main/java/de/kyle/gefangenendilemma/competition/config/CompetitionSpecification.java
+++ src/main/java/de/kyle/gefangenendilemma/competition/config/CompetitionSpecification.java
@@ -17,17 +17,17 @@ public class CompetitionSpecification {
         Properties properties = new Properties();
         File specificationFolder = new File("specification");
         if (specificationFolder.mkdir()) {
-            properties.load(getClass().getResourceAsStream("DefaultCompetitionSpecification.properties"));
+            properties.load(getClass().getResourceAsStream("/DefaultCompetitionSpecification.properties"));
         } else {
             File[] configs = specificationFolder.listFiles((dir, name) -> name.endsWith(".properties"));
             if (configs == null || configs.length == 0) {
-                properties.load(getClass().getResourceAsStream("DefaultCompetitionSpecification.properties"));
+                properties.load(getClass().getResourceAsStream("/DefaultCompetitionSpecification.properties"));
             } else {
                 properties.load(new FileInputStream(configs[0]));
             }
         }
-        rounds = (int) properties.get("competition.rounds");
-        cooperatePoints = (int) properties.get("competition.points.cooperate");
-        betrayPoints = (int) properties.get("competition.points.betray");
+        rounds = Integer.parseInt(properties.getProperty("competition.rounds"));
+        cooperatePoints = Integer.parseInt(properties.getProperty("competition.points.cooperate"));
+        betrayPoints = Integer.parseInt(properties.getProperty("competition.points.betray"));
     }
 }
